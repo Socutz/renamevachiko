e19f174fbc29d5944f7d947c94e7f56b93c74c45
Socutz <122589605+Socutz@users.noreply.github.com>
2023-01-16 09:22:06 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -123,8 +123,8 @@ async def cp3(bot,update):
 async def dft(bot,update):
 	id = update.message.reply_to_message.text.split("/resetpower")
 	user_id = id[1].replace(" ", "")
-	inlimit = 1288490188
-	uploadlimit(int(user_id), 1288490188)
+	inlimit = 4294967296
+	uploadlimit(int(user_id), 4294967296)
 	usertype(int(user_id),"**Free**")
 	addpre(int(user_id))
 	await update.message.edit("Daily Data limit has been reset successsfully.\nThis account has default 1.2 GB renaming capacity ")
